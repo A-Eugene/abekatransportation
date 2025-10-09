b0a94afee56e3806fe7bbe194ca7876ef66a090c
A-Eugene <amadeuseugenedirgantara@gmail.com>
2025-10-09 15:07:24 +0700

    Eugene - Jae bikin navbar sama footer

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-sm bg-white border-bottom">
+  <div class="container-fluid d-flex flex-column align-items-center">
+
+    <div class="w-100 d-flex justify-content-start mb-2">
+      <a class="navbar-brand" href="/">
+        <img src="{{ asset('images/logo-trans.png') }}" alt="Logo" style="width: 120px;">
+      </a>
+    </div>
+
+    <div class="w-100 d-flex justify-content-center">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link 1</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link 2</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link 3</a>
+        </li>
+      </ul>
+    </div>
+
+  </div>
+</nav>
--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -5,6 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>@yield("title")</title>
   <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+  <link rel="icon" href="{{ asset('images/logo-trans.png') }}" type="image/png">
 </head>
 <body>
   @include('components.navbar')
