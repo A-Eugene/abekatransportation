9d3e5da031594c618163be4aadb2725dfcaaec57
A-Eugene <amadeuseugenedirgantara@gmail.com>
2025-10-09 16:21:28 +0700

    Jumbotron Commit

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -5,11 +5,13 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>@yield("title")</title>
   <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/theme.css') }}">
   <link rel="icon" href="{{ asset('images/logo-trans.png') }}" type="image/png">
 </head>
 <body>
-  @include('components.navbar')
-  @include('components.footer')
+    @include('components.navbar')
+    @yield('content')
+    @include('components.footer')
 </body>
 <script src="{{ asset('js/bootstrap.js') }}"></script>
 </html>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,3 +1,30 @@
 @extends('layouts.landing')
 
 @section('title', 'Abeka Transportation')
+
+@section('content')
+
+<div class="jumbotron container-fluid position-relative bg-cover bg-bottom bg-fixed bg-no-repeat">
+  <div class="overlay position-absolute top-0 start-0 w-100 h-100"></div>
+
+  <div class="container position-relative text-white text-center d-flex flex-column justify-content-center h-100">
+      <h1 class="fw-bold display-4">Solusi Terpercaya untuk Pengiriman Anda</h1>
+      <p class="lead mt-3">
+          Abeka Transportation — Mitra ekspedisi andalan sejak 1994, melayani pengiriman antar kota dengan aman dan tepat waktu.
+      </p>
+      <a href="#layanan" class="btn btn-lg theme-button">Pelajari Layanan Kami</a>
+  </div>
+</div>
+
+@endsection
+
+<style>
+    .jumbotron {
+        background-image: url('{{ asset('images/jumbotron-test.jpg') }}');
+        height: 80vh;
+    }
+
+    .jumbotron .overlay {
+        background-color: rgba(0, 0, 0, 0.5);
+    }
+</style>
