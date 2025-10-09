3bcb77583b3cea34f557403da4e4d1c8394ae70b
A-Eugene <amadeuseugenedirgantara@gmail.com>
2025-10-09 16:28:02 +0700

    Merge 1

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -7,7 +7,7 @@
 <div class="jumbotron container-fluid position-relative bg-cover bg-bottom bg-fixed bg-no-repeat">
   <div class="overlay position-absolute top-0 start-0 w-100 h-100"></div>
 
-  <div class="container position-relative text-white text-center d-flex flex-column justify-content-center h-100">
+  <div class="container position-relative text-white text-center d-flex flex-column justify-content-center min-h-100">
       <h1 class="fw-bold display-4">Solusi Terpercaya untuk Pengiriman Anda</h1>
       <p class="lead mt-3">
           Abeka Transportation — Mitra ekspedisi andalan sejak 1994, melayani pengiriman antar kota dengan aman dan tepat waktu.
