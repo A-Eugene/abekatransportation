f9d1b9dede948ada774c998239e0c92f23620c08
joevandy <kochenkbiru@gmail.com>
2025-10-13 18:37:17 +0700

    update view profile (2 new blade file) -jae

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,9 @@ Route::get('/', [LandingController::class, 'beranda']);
 
 Route::get('/profil-perusahaan', [LandingController::class, 'profilPerusahaan']);
 
+Route::get('/informasi', [LandingController::class, 'informasiPerusahaan']);
+
+Route::get('/informasi-paket', [LandingController::class, 'informasiPaket']);
+
+Route::get('/informasi-syaratketentuan', [LandingController::class, 'informasiSyaratKetentuan']);
+
